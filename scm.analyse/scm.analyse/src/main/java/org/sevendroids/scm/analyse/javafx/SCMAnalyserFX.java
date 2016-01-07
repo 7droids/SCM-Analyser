47b6d490d92a0f062d17ad92cb1672f908233630
Fridtjof Ahlswede <to.7droids@gmail.com>
2016-01-07 17:48:37 +0100

    ADD: Main frame for the GUI including a panel to enter the necessary parameter for the analyser.
    Signed-off-by: Fridtjof Ahlswede <to.7droids@gmail.com>

---
+++ scm.analyse/scm.analyse/src/main/java/org/sevendroids/scm/analyse/javafx/SCMAnalyserFX.java
@@ -0,0 +1,55 @@
+package org.sevendroids.scm.analyse.javafx;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class SCMAnalyserFX extends Application {
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("SCM Analyser");
+
+		initRootLayout();
+
+		showParameterView();
+	}
+
+	private void initRootLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(SCMAnalyserFX.class.getResource("view/RootLayout.fxml"));
+			rootLayout = loader.load();
+
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void showParameterView() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(SCMAnalyserFX.class.getResource("view/ParameterView.fxml"));
+			AnchorPane pane = loader.load();
+
+			rootLayout.setCenter(pane);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
